b8518cce1f8e734234ce4179094fe593f67b965e
DanielLarsen-spec <larsen97daniel@gmail.com>
2024-08-29 12:47:22 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function knapp(color, Id){
+let div = document.getElementById("buttons")
+let currentDiv = document.getElementById(Id)
+
+div.style.background = color;
+
+currentDiv.classList.toggle("border")
+
+
+
+
+
+    
+    
+    
+}
+
+
+
+
+
+
+    
+    
+    
